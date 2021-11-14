1d7873a4c509bd54149732e47f20436affbf9deb
sank48 <sanksrn48@gmail.com>
2021-11-14 23:54:43 +0530

    Added Sign up Modal

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
-	$('.navbar-text').click(function() {
+	$('.login').click(function() {
 		$('#loginModal').modal('show');
-	})
+	});
+	$('.signup').click(function(){
+		$('#signupModal').modal('show');
+	});
 	$('.close').click(function() {
 		$('#loginModal').modal('hide');
-	})
+		$('#signupModal').modal('hide');
+	});
 })

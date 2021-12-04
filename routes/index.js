b6f3c4591c847067503fe244575b0f0421de4516
sank48 <sanksrn48@gmail.com>
2021-12-04 20:06:29 +0530

    Added a basic server

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const http = require('http');
+const path = require('path');
+
+const host = "localhost";
+const port = "3000";
+const staticPath = path.join(__dirname,"../");
+// app.use((req, res, next) =>{
+//   res.statusCode = 200;
+//   res.send('index.html');
+// });
+app.use(express.static(staticPath));
+console.log(staticPath);
+
+app.get('/',(req,res)=>{
+  res.sendFile(root)
+})
+
+const server = http.createServer(app);
+
+server.listen(port, host, ()=>{
+  console.log(`Server running at http://${host}:${port}`);
+});

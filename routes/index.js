3f3b4087dd21169e98bae5d90f88f5ac9c44b8ed
sank48 <sanksrn48@gmail.com>
2022-01-06 17:43:44 +0530

    Added post method using body-parser

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const host = "localhost";
 const port = "3000";
@@ -15,15 +16,37 @@ const logger=(req,res,next)=>{
   console.log(req.method,req.path, '-', req.ip );
   next();
 }
-app.use(logger);
 
+
+// a logger to log method, path and ip
+app.use(logger);
 app.use(express.static(staticPath));
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
 // console.log(staticPath);
 
 app.get('/',(req,res)=>{
   res.sendFile(root)
 })
 
+
+app.post('/signin',(req,res,next)=>{
+  res.json({
+    "Email": req.body["email"],
+    "Password": req.body["password"]
+  });
+  return
+})
+app.post('/signup',(req,res,next)=>{
+  res.json({
+    "Full name": req.body["fullname"],
+    "Username":req.body.username,
+    "Email": req.body["email"],
+    "Password":req.body.passwords,
+    "Confirm Password": req.body.Cpasswords
+  });
+  return
+})
 const server = http.createServer(app);
 
 server.listen(port, host, ()=>{
